8528e3877d487ae0e36d961b179062cd0901504c
Nargiza <nnematova7250@gmail.com>
2025-07-18 10:50:26 +0100

    Full pipeline with targets for assessment

--- _targets.R
+++ _targets.R
@@ -1,25 +1,64 @@
 library(targets)
 library(tarchetypes)
-library(here)
 
-source(here("R", "functions.R"))
-source(here("R", "load_data.R"))
-source(here("R", "utils.R"))
+tar_option_set(
+  packages = c("DBI", "RPostgres", "dplyr", "ggplot2", "caret", "randomForest",
+               "nnet", "pROC", "forcats", "tidyr", "factoextra", "cluster"),
+  format = "rds"
+)
 
-
-tar_option_set(packages = c("dplyr", "DBI", "RPostgres"))
 
 list(
-  tar_target(
-    raw_data,
-    load_data()
-  ),
-  tar_target(
-    summary_data,
-    summarise_data(raw_data)
-  ),
-  tar_render(
-    report,
-    "vignettes/Report.Rmd"
-  )
+ 
+  tar_target(classification_data,
+             {
+               source("R/classification.R")
+               data_clean
+             }),
+  
+  tar_target(rf_model,
+             {
+               source("R/classification.R")
+               rf_model
+             }),
+  
+  tar_target(mlogit_model,
+             {
+               source("R/classification.R")
+               mlogit_model
+             }),
+  
+  
+ 
+  tar_target(regression_data,
+             {
+               source("R/regression.R")
+               df_model
+             }),
+  
+  tar_target(regression_model,
+             {
+               source("R/regression.R")
+               multinom_model
+             }),
+  
+  
+
+  tar_target(olive_data,
+             {
+               source("R/unsupervised.R")
+               olive_scaled
+             }),
+  
+  tar_target(clustering_result,
+             {
+               source("R/unsupervised.R")
+               kmeans_result
+             }),
+  
+  tar_target(silhouette_score,
+             {
+               source("R/unsupervised.R")
+               mean_sil
+             })
 )
